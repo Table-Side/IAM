30d30b127e1438a235fc8e21d27d74429591836c
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-01 14:15:12 +0100

    Add more logs to entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -19,6 +19,7 @@ if [ -n "$KEYCLOAK_HOST" ] && \
     LE_RENEW_CRON_COMMAND=$(eval echo $LE_RENEW_CRON_COMMAND)
 
     # Disable Keycloak config first as cert not present.
+    echo "Disabling Keycloak config..."
     mv -v /etc/nginx/conf.d/keycloak.conf /etc/nginx/conf.d/keycloak.conf.disabled
 
     (
@@ -31,12 +32,14 @@ if [ -n "$KEYCLOAK_HOST" ] && \
             --webroot -w /usr/share/nginx/html -d $KEYCLOAK_DOMAIN
 
         # Enable Keycloak config
+        echo "Re-Enabling Keycloak config with SSL certificate..."
         mv -v /etc/nginx/conf.d/keycloak.conf.disabled /etc/nginx/conf.d/keycloak.conf
 
         echo "Reloading NGINX with SSL..."
         nginx -s reload
 
         # Install crontab for cert renewal
+        echo "Installing crontab for automated certificate renewal..."
         touch crontab.tmp \
             && echo "37 2 * * * certbot renew ${LE_RENEW_OPTIONS} --post-hook 'nginx -s reload' && ${LE_RENEW_CRON_COMMAND} > /dev/null 2>&1" > crontab.tmp \
             && crontab crontab.tmp \
